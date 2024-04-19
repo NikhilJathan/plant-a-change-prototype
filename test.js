8055543b0eebff67248a3ac774654362925ecc39
ShamanDorn <98718010+NikhilJathan@users.noreply.github.com>
2024-04-19 18:17:34 +1000

    added in basic AI generation to test!!

--- test.js
+++ test.js
@@ -1,5 +1,27 @@
-async function main() {    
-    document.getElementById("openai").innerHTML = "testttttt";
+const apiKey = process.env.OPENAI_API_KEY;
+
+async function main() {
+    const response = await axios.post(
+        "https://api.openai.com/v1/completions",
+        {
+          prompt: "what plants are native to the Greensquare area in Sydney, Australia",
+          model: "text-davinci-003",
+          temperature: 0,
+          max_tokens: 1000,
+          top_p: 1,
+          frequency_penalty: 0.0,
+          presence_penalty: 0.0,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${apiKey}`,
+          },
+        }
+      );
+      const chatbotResponse = response.data.choices[0].text;
+
+    document.getElementById("openai").innerHTML = chatbotResponse;
   }
   
   main();
