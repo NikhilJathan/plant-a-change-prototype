ccf9f8e2884f52cd07d29568464a81e59e49f059
ShamanDorn <98718010+NikhilJathan@users.noreply.github.com>
2024-04-20 22:16:49 +1000

    updated script to "generate" text for prototype

--- grapevine.js
+++ grapevine.js
@@ -1,3 +1,3 @@
 document.getElementById("search-button").addEventListener("click", function() {
-    document.getElementById('openai').innerHTML = '<ol><li>html data</li></ol>';
+    document.getElementById('openai').innerHTML = '<div class="card"><div class="card-body">This is some text within a card body.</div></div>';
   });
