ad4db78accde8d845f6921ad056f9727d2570d71
ShamanDorn <98718010+NikhilJathan@users.noreply.github.com>
2024-04-21 13:10:51 +1000

    Update grapevine.js

--- grapevine.js
+++ grapevine.js
@@ -1,3 +1,3 @@
 document.getElementById("search-button").addEventListener("click", function() {
-    document.getElementById('openai').innerHTML = '<div class="card"><div class="card-body">This is some text within a card body.</div></div>';
+    document.getElementById('openai').innerHTML = '<div class="card"><div class="card-body">This feature is currently unavailable, due to technical limitations with OpenAI, where we need to set up extra servers to ensure the security of the OpenAI key.</div></div>';
   });
